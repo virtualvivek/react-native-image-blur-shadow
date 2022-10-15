3ca7e36cf8dbe26098b7ae669d0cbc38f3c901ca
Vivek Verma <virtualiz.me@gmail.com>
2022-10-16 01:12:11 +0530

    Update App.js

--- example/App.js
+++ example/App.js
@@ -20,71 +20,69 @@ class App extends React.Component {
             backgroundColor: '#fff',
             marginBottom: 25
           }}>
-        <View style={styles.header}>
-          <Text style={styles.header_text}>react-native-image-blur-shadow</Text>
-        </View>
+          <View style={styles.header}>
+            <Text style={styles.header_text}>react-native-image-blur-shadow</Text>
+          </View>
 
-        <ImageBlurShadow
-          style={styles.img}
-          source={require('./src/assets/spiderman.jpg')}
-          imageWidth={220}
-          imageHeight={220}
-          imageBorderRadius={22}
-          shadowOffset={48}
-          shadowBlurRadius={18}
-          shadowBackgroundColor={'#ffffff'}
-        />
-       
+          <ImageBlurShadow
+            style={styles.img}
+            source={require('./src/assets/spiderman.jpg')}
+            imageWidth={220}
+            imageHeight={220}
+            imageBorderRadius={22}
+            shadowOffset={48}
+            shadowBlurRadius={18}
+            shadowBackgroundColor={'#ffffff'}
+          />
 
-        <ImageBlurShadow
-          style={styles.img}
-          source={require('./src/assets/car.jpg')}
-          imageWidth={200}
-          imageHeight={200}
-          imageBorderRadius={22}
-          shadowOffset={42}
-          shadowBlurRadius={this.state.blur_value}
-          shadowBackgroundColor={'#ffffff'}
-        />
+          <ImageBlurShadow
+            style={styles.img}
+            source={require('./src/assets/car.jpg')}
+            imageWidth={200}
+            imageHeight={200}
+            imageBorderRadius={22}
+            shadowOffset={42}
+            shadowBlurRadius={this.state.blur_value}
+            shadowBackgroundColor={'#ffffff'}
+          />
 
-      <View style={styles.slider_style_con}>
-        <Slider
-          style={{width:'50%'}}
-          step={1}
-          minimumValue={1}
-          maximumValue={50}
-          value={this.state.blur_value}
-          onSlidingComplete={blur_value => this.setState({blur_value: blur_value})}/>
-        <Text>Blur Radius {this.state.blur_value}</Text>
-      </View>
-        
+          <View style={styles.slider_style_con}>
+            <Slider
+              style={{width:'50%'}}
+              step={1}
+              minimumValue={1}
+              maximumValue={50}
+              value={this.state.blur_value}
+              onSlidingComplete={blur_value => this.setState({blur_value: blur_value})}/>
+            <Text>Blur Radius {this.state.blur_value}</Text>
+          </View>
 
-      <ImageBlurShadow
-          style={styles.img}
-          source={require('./src/assets/landscape.png')}
-          imageWidth={250}
-          imageHeight={200}
-          imageBorderRadius={12}
-          shadowBlurRadius={12}
-          shadowBackgroundColor={'#ffffff'}
-        />
+          <ImageBlurShadow
+              style={styles.img}
+              source={require('./src/assets/landscape.png')}
+              imageWidth={250}
+              imageHeight={200}
+              imageBorderRadius={12}
+              shadowBlurRadius={12}
+              shadowBackgroundColor={'#ffffff'}
+            />
 
-      <ImageBlurShadow
-          style={styles.img}
-          source={require('./src/assets/one.jpg')}
-          imageWidth={265}
-          imageHeight={200}
-          imageBorderRadius={0}
-          shadowOffset={58}
-          shadowBlurRadius={10}
-          shadowBackgroundColor={'#ffffff'}
-        />
+          <ImageBlurShadow
+              style={styles.img}
+              source={require('./src/assets/one.jpg')}
+              imageWidth={265}
+              imageHeight={200}
+              imageBorderRadius={0}
+              shadowOffset={58}
+              shadowBlurRadius={10}
+              shadowBackgroundColor={'#ffffff'}
+            />
 
-        <View style={styles.footer}>
-          <Text>Developed By: virtualvivek</Text>
-          <Text>Github: virtualvivek/react-native-image-blur-shadow</Text>
-          <Text>Supports Android, iOS, and Web</Text>
-        </View>
+          <View style={styles.footer}>
+            <Text>Developed By: virtualvivek</Text>
+            <Text>Github: virtualvivek/react-native-image-blur-shadow</Text>
+            <Text>Supports Android, iOS, and Web</Text>
+          </View>
         
         </View>
       </ScrollView>
